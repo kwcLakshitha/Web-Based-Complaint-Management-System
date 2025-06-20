53629c44238ac702744f66b2a501035d7dd95e5d
kwcLakshitha <your-email@example.com>
2025-06-20 16:19:53 +0530

    signin done

--- cms/src/main/java/edu/ijse/cms/controler/SignInServlet.java
+++ cms/src/main/java/edu/ijse/cms/controler/SignInServlet.java
@@ -1,5 +1,84 @@
 package edu.ijse.cms.controler;
 
-public class SignInServlet {
-    
+import edu.ijse.cms.dto.UserDto;
+import edu.ijse.cms.model.SignInModel;
+import jakarta.annotation.Resource;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/signin")
+@MultipartConfig
+public class SignInServlet extends HttpServlet {
+
+    @Resource(name = "java:comp/env/jdbc/pool")
+    DataSource dataSource;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userEmail = request.getParameter("userEmail");
+        String userPassword = request.getParameter("userPassword");
+
+
+        System.out.println("userEmail: " + userEmail);
+        System.out.println("userPassword: " + userPassword);
+
+        SignInModel signInModel = new SignInModel();
+
+
+        Connection connection = null;
+        try {
+
+
+            connection = dataSource.getConnection();
+
+            System.out.println("no problem");
+            ResultSet rs = signInModel.userLogin(userEmail, connection);
+            UserDto user = new UserDto();
+
+            List<UserDto> userList = new ArrayList<>();
+
+            while (rs.next()) {
+                user.setUserId(rs.getString("id"));
+                user.setUserName(rs.getString("user_name"));
+                user.setUserEmail(rs.getString("user_email"));
+                user.setUserRoll(rs.getString("user_roll"));
+                user.setPassword(rs.getString("password"));
+            }
+
+            System.out.println("no problem..............");
+
+            System.out.println(user.getUserEmail());
+            System.out.println(user.getUserName());
+            System.out.println(user.getUserRoll());
+
+
+        if (user.getPassword().equals(userPassword)) {
+            System.out.println("login successful");
+
+            if ("admin".equals(user.getUserRoll())) {
+                System.out.println("admin dashboard .......");
+            } else {
+                System.out.println("another dashboard .......");
+            }
+        } else {
+            System.out.println("Incorrect password");
+        }
+
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
---
+++ cms/src/main/java/edu/ijse/cms/model/SignInModel.java
@@ -0,0 +1,26 @@
+package edu.ijse.cms.model;
+
+import edu.ijse.cms.dto.UserDto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SignInModel {
+    public ResultSet userLogin(String userEmail, Connection connection) throws SQLException {
+
+        System.out.println("signInModel.userLogin");
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE user_email=?");
+        preparedStatement.setString(1, userEmail);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+//        System.out.println("query no problem");
+//        UserDto userDto = new UserDto(resultSet.getString("id"), resultSet.getString("user_name"),
+//                resultSet.getString("user_email"), resultSet.getString("user_roll"),
+//                resultSet.getString("password"), resultSet.getString("password"));
+
+        System.out.println("set data in dto");
+        return resultSet;
+    }
+}
